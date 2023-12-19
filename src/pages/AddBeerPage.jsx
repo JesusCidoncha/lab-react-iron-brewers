c4eca3de42d08064895154333e23e7fd8a1b56a9
Jesus Cidoncha <jesus.cidoncha.berlanga@gmail.com>
2023-12-19 17:19:51 +0100

    iterations 1- 3

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { Link } from "react-router-dom/dist";
 
-function AddBeerPage() {
+const AddBeerPage = () => {
   // State variables to store the values of the form inputs. You can leave these as they are.
   const [name, setName] = useState("");
   const [tagline, setTagline] = useState("");
@@ -21,20 +22,49 @@ function AddBeerPage() {
   const handleAttenuationLevel = (e) => setAttenuationLevel(e.target.value);
   const handleContributedBy = (e) => setContributedBy(e.target.value);
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const payload = {
+      name,
+      tagline,
+      description,
+      imageUrl,
+      firstBrewed,
+      brewersTips,
+      attenuationLevel,
+      contributedBy,
+    };
 
-
+    try {
+      const response = await fetch(
+        `https://ih-beers-api2.herokuapp.com/beers/new`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        }
+      );
+      if (response.status === 201) {
+        const newBeer = await response.json();
+        console.log(newBeer);
+        navigate(`/projects/${newBeer.id}`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   // TASK:
   // 1. Create a function to handle the form submission and send the form data to the Beers API to create a new beer.
   // 2. Use axios to make a POST request to the Beers API.
   // 3. Once the beer is created, navigate the user to the page showing the list of all beers.
 
-
-
   // Structure and the content of the page showing the form for adding a new beer. You can leave this as it is.
   return (
     <>
       <div className="d-inline-flex flex-column w-100 p-4">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Name</label>
           <input
             className="form-control mb-4"
@@ -122,11 +152,15 @@ function AddBeerPage() {
             value={contributedBy}
             onChange={handleContributedBy}
           />
-          <button className="btn btn-primary btn-round">Add Beer</button>
+          <Link to="/beers">
+            <button type="submit" className="btn btn-primary btn-round">
+              Add Beer
+            </button>
+          </Link>
         </form>
       </div>
     </>
   );
-}
+};
 
 export default AddBeerPage;
